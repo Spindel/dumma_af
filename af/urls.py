f09005471a679385617630e0736171951d0f747e
DmD (Spider) Ljungmark <darkmere@gmail.com>
2013-08-15 21:34:31 +0200

    Get the views to work for the log

--- af/urls.py
+++ af/urls.py
@@ -2,5 +2,8 @@
 from af import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^log/?$', views.log, name='log'),
+    url(r'^log/user/(?P<userid>\d+)$', views.userLog, name='userlog'),
+    url(r'^log/email/(?P<email>\d+)$', views.emailLog, name='emaillog'),
 )
--- af/views.py
+++ af/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 def index(request):
         return HttpResponse("Hello, world.  This is the index")
 
+def log(request):
+        return HttpResponse("This is the logs section")
+
+def userLog(request, userid):
+    return HttpResponse("This should return the log for user %s" % userid)
+
+
+def emailLog(request, email):
+    return HttpResponse("This should return the log for email %s" % email)
