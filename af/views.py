fdec1a6bae3b3dee22fb534f5af1d4c63d0c7e96
DmD (Spider) Ljungmark <darkmere@gmail.com>
2013-08-16 17:07:08 +0200

    Move to external templates

--- af/views.py
+++ af/views.py
@@ -2,10 +2,17 @@
 
 from django.http import HttpResponse
 
-from af.models import Log
+from af.models import Corporation, Log
+
+from django.shortcuts import render
+
 
 def index(request):
-        return HttpResponse("This is the index function")
+    corporation_list = Corporation.objects.all()
+    context = {
+        'corporation_list': corporation_list,
+    }
+    return render(request, 'af/index.html', context)
 
 def log(request):
     last_log = Log.objects.order_by('-date')[:5]
@@ -18,3 +25,12 @@ def userLog(request, userid):
 
 def emailLog(request, email):
     return HttpResponse("This should return the log for email %s" % email)
+
+from django.http import Http404
+
+def detail(request, logId):
+    try:
+        poll = Poll.objects.get(lg=log_id)
+    except Log.DoesNotExist:
+        raise Http404
+    return render(request, 'af/detail.html', {'log':log})
